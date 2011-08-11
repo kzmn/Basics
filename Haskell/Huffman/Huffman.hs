fb31a0c6b2f621343192a60ce81619ce6c87cdbf
kzmn <kzmn@vt.edu>
2011-08-11 13:17:50 -0400

    Began Huffman encoding project, got Added Bit, HuffCode, HuffTable, and HuffTree types along with the getWeight function to Huffman_Utils.hs, also added a basic opening comment to Huffman.hs.

---
+++ Haskell/Huffman/Huffman.hs
@@ -0,0 +1,17 @@
+{- |
+Module			:	Huffman.hs
+Description		:	This file creates basic functions for Huffman encoding.
+Copyright		:	(c) Copyright 2011 Richard Easterling. All Rights Reserved. 
+License			:	Released under the MIT license.
+
+Maintainer		:	thegreatkzmn@gmail.com
+Stability		:	unstable
+Portability		:	portable
+
+
+-}
+
+module Huffman
+()
+where
+    
---
+++ Haskell/Huffman/Huffman_Utils.hs
@@ -0,0 +1,58 @@
+{- |
+Module			:	Huffman_Types.hs
+Description		:	This file creates basic types for Huffman encoding.
+Copyright		:	(c) Copyright 2011 Richard Easterling. All Rights Reserved. 
+License			:	Released under the MIT license.
+
+Maintainer		:	thegreatkzmn@gmail.com
+Stability		:	unstable
+Portability		:	portable
+
+
+-}
+module Huffman_Utils
+(Bit,
+HuffCode,
+HuffTable,
+HuffTree,
+getWeight)
+where
+    
+{- |
+Type: Bit
+The Bit type is used to represent the bits that will be used in the encoding
+scheme.  We will use the characters L and R rather than the traditional 1 and
+0 here but will convert to a proper bytetream when writing to a file.
+-}
+data Bit = L | R
+         deriving (Eq, Show)
+
+{- |
+Type: HuffCode
+This type represents a huffman code, which is simply a list of bits.
+-}
+type HuffCode   = [Bit]
+
+type HuffTable  = [(Char, HuffCode)]
+
+{- |
+Data Type: HuffTree
+This data type defines the tree structure used by the huffman encoding
+algorithm.  A tree can be an EmptyTree which is simply nothing, a Leaf
+which has an integer weight and a character from the alphabet that is being
+encoded, and finally a node that has an integer weight and two sub trees.
+-}
+data HuffTree   = EmptyTree
+                | Leaf Int Char
+                | Node Int HuffTree HuffTree
+                deriving(Show, Read)
+   
+{- |
+Function: getWeight
+This function simply returns the weight of the given tree.
+-}             
+getWeight :: HuffTree a -> Int
+getWeight EmptyTree = 0
+getWeight (Leaf weight char) = weight
+getWeight (Node weight leftChild rightChild) = weight
+
